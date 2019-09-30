71ccd5fa6d4330ff8715d5ba903ea3547defaf5c
Kamal Reddy <kamalreddy4195gmail.com>
2019-09-30 12:24:28 -0500

    upaded code in html and js

---
+++ index.js
@@ -0,0 +1,6 @@
+function calculate() {
+    let a =parseInt(document.getElementById("Number").value);
+    let total=a+2;
+    document.getElementById("result").innerHTML ="People per course:" +total;
+  }
+  
